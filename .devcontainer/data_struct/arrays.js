ef009a3f591bf9dd8a247bb2508942eea941bad8
blitz254 <139634535+blitz254@users.noreply.github.com>
2025-06-29 06:09:50 +0000

    arrays

--- .devcontainer/data_struct/arrays.js
+++ .devcontainer/data_struct/arrays.js
@@ -39,3 +39,38 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)   // returns [] but by manipulating the string, it takes out [,] from the string
 console.log("C ", myArr);
 console.log(myn2);
+
+
+
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)    // pushes dc array as an element
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)    // concat returns on a new array 
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]   // spread
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)             // returns less dimensional array
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
