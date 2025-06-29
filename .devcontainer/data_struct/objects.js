8e90365ce84bb35b115833d02b6f160f4dc71834
blitz254 <139634535+blitz254@users.noreply.github.com>
2025-06-29 06:39:01 +0000

    object

---
+++ .devcontainer/data_struct/objects.js
@@ -0,0 +1,41 @@
+// if literals are declared, singleton is not formed but if a constructor is declared, singleton is formed
+//object.create   //this is constructor method
+
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",        //mySym: "mykey1"   --> acts as string
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+console.log(JsUser.greeting());  // returns the function reference 
+console.log(JsUser.greeting());  // return the content of function
+console.log(JsUser.greetingTwo());
