fea067a908a42dacc778e0f1c0ad6a9af0edadd8
blitz254 <139634535+blitz254@users.noreply.github.com>
2025-07-02 06:46:50 +0000

    filter maps reduce

---
+++ iterations/reduce.js
@@ -0,0 +1,53 @@
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);     // initial value is used for the first time only, to initialise the accumulator
+
+// console.log(sumWithInitial);
+// Expected output: 10
+
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
+
+
+// Why use reduce()?
+// It’s flexible: you can use it to implement map, filter, count, sum, max, etc.
+// It avoids creating intermediate arrays (like map or filter do).
+// Makes code more declarative (especially for data transformation)
